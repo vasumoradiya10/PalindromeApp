1633c0ac9cc8e8986d1cd06f2e10fa3d4d44469d
Vasu <vasumoradiya2007@gmail.com>
2026-02-25 15:08:25 +0530

    uc 1 - print welcome message

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the palindrome checker management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }}
